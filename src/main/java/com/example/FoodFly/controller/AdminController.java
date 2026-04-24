f2fbaf83357c8d59ddaf50368fc0fbecf52b5364
siddhi-ramane"ramanesiddhi9@gmail.com <ramanesiddhi9@gmail.com>
2026-04-25 02:54:27 +0530

    ignordsfsd

--- src/main/java/com/example/FoodFly/controller/AdminController.java
+++ src/main/java/com/example/FoodFly/controller/AdminController.java
@@ -7,12 +7,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.FoodFly.enitity.MenuItems;
 import com.example.FoodFly.service.AdminService;
 
-
+@RestController
+@RequestMapping("/admin")
 public class AdminController {
 
 	@Autowired
--- src/main/java/com/example/FoodFly/controller/AsignRoleController.java
+++ src/main/java/com/example/FoodFly/controller/AsignRoleController.java
@@ -16,8 +16,8 @@ import com.example.FoodFly.enitity.AssignRole;
 import com.example.FoodFly.service.AssignRoleService;
 
 @RestController
-@RequestMapping("/Role")
-@CrossOrigin(origins = "https://food-fly-frontend-three.vercel.app")
+@RequestMapping("/role")
+
 public class AsignRoleController {
 
 	@Autowired
@@ -35,7 +35,7 @@ public class AsignRoleController {
 		List<AssignRole> a = s.getallrole();
 		return ResponseEntity.ok(a);
 	}
-		@PostMapping("/Login")
+		@PostMapping("/login")
 		public ResponseEntity<AssignRole> loginRole(@RequestBody AssignRole a){
 			AssignRole as = s.loginassignservice(a);
 		return ResponseEntity.ok(as);
