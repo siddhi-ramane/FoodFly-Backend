0e204a5700662b891fc60a03ef34e740ad0fe80e
siddhi-ramane"ramanesiddhi9@gmail.com <ramanesiddhi9@gmail.com>
2026-04-25 02:13:57 +0530

    all fixed finall

--- src/main/java/com/example/FoodFly/security/CorsConfig.java
+++ src/main/java/com/example/FoodFly/security/CorsConfig.java
@@ -1,24 +1,18 @@
 package com.example.FoodFly.security;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
+public class CorsConfig implements WebMvcConfigurer {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOriginPatterns("https://food-fly-frontend-three.vercel.app")
-                        .allowedMethods("*")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("https://food-fly-frontend-three.vercel.app")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
