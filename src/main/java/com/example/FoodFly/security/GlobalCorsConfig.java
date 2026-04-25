e60b3fdf93e25a04f63716803a99e602971603af
siddhi-ramane"ramanesiddhi9@gmail.com <ramanesiddhi9@gmail.com>
2026-04-25 12:11:42 +0530

    pls fixing italll

---
+++ src/main/java/com/example/FoodFly/security/GlobalCorsConfig.java
@@ -0,0 +1,29 @@
+package com.example.FoodFly.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class GlobalCorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                  
+                        .allowedOrigins("https://food-fly-frontend-three.vercel.app")
+
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+
+                        .allowedHeaders("*")
+
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
--- src/main/java/com/example/FoodFly/security/package-info.java
+++
@@ -1,1 +0,0 @@
-package com.example.FoodFly.security;
