ecba0258bfc95d8d53e58cea8bec41d68658c2a8
siddhi-ramane"ramanesiddhi9@gmail.com <ramanesiddhi9@gmail.com>
2026-05-18 22:32:04 +0530

    fixed security config

--- src/main/java/com/example/FoodFly/config/SecurityConfig.java
+++ src/main/java/com/example/FoodFly/config/SecurityConfig.java
@@ -2,24 +2,14 @@ package com.example.FoodFly.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class SecurityConfig {
-
-    private final WebMvcConfigurer corsConfigurer;
-
-    SecurityConfig(WebMvcConfigurer corsConfigurer) {
-        this.corsConfigurer = corsConfigurer;
-    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -27,11 +17,10 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http,
-                                          CorsConfigurationSource corsConfigurationSource) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
-            .cors(cors -> cors.configurationSource(corsConfigurationSource))
+            .cors(Customizer.withDefaults())
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
                 .anyRequest().permitAll()
@@ -39,6 +28,4 @@ public class SecurityConfig {
 
         return http.build();
     }
-
-
 }
